95e5a567982971cd150cd6ce8a31f7bf528a5099
SOVON SINGHA <50170438+sovon9@users.noreply.github.com>
2024-05-18 08:51:33 +0530

    cloud config server

---
+++ SPRING-CLOUD-CONFIG/src/test/java/com/configserver/SPRINGCLOUDCONFIG/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configserver.SPRINGCLOUDCONFIG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
